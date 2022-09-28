bf8201959048f75b06a1d48e0e937784e97c80b2
erebuseternal <marcelsanders96@gmail.com>
2022-09-28 12:45:49 +0000

    map transforms 1

--- buggy/transform.py
+++ buggy/transform.py
@@ -5,24 +5,75 @@ from .kobo import Kobo
 def mapping_transform(entry_key, output_key, mapping, default, entry):
     return output_key, mapping.get(entry.get(entry_key), default)
 
-survey_method_entry_key = "session_info/survey_method"
-survey_method_output_key = "survey_method"
-survey_method_mapping = {
-    "incidental": "Opportunistic encounter",
-    "walking": "Semi-structured survey",
-    "transect": "Transect survey",
-    "area": "Area search",
-    "other": "Other"
-}
-survey_method_default = "Other"
-survey_method_transform = partial(
-    mapping_transform, survey_method_entry_key,
-    survey_method_output_key, survey_method_mapping, 
-    survey_method_default
+survey_transform = partial(
+    mapping_transform, 
+    "session_info/survey_method",
+    "EwA - Survey Method",
+    {
+        "incidental": "Opportunistic encounter",
+        "walking": "Semi-structured survey",
+        "transect_survey": "Transect survey",
+        "area": "Area search",
+        "other": "Other"
+    },
+    "Other"
+)
+
+development_transform = partial(
+    mapping_transform,
+    "arthropod_documentation/developmental_stage",
+    "EwA - Arthropod Developmental Stage",
+    {
+        "adult": "Adult",
+        "egg": "Egg or eggsac",
+        "larva": "Larva or caterpillar",
+        "pupa": "Pupa",
+        "nymph": "Nymph",
+        "other": "Other"
+    },
+    "Other"
+)
+
+activity_transform = partial(
+    mapping_transform,
+    "arthropod_documentation/activity",
+    "EwA - Behavior Observed",
+    {
+        "mating": "Mating",
+        "moving": "Moving",
+        "foraging": "Foraging",
+        "feeding": "Feeding",
+        "resting": "Resting",
+        "predator": "Preying",
+        "prey": "Preyed upon",
+        "guarding": "Guarding eggs or younglings",
+        "tending": "Tending",
+        "other": "Other"
+    },
+    "Other"
+)
+
+host_phenology_transform = partial(
+    mapping_transform,
+    "host_documentation/host_phenology",
+    "EwA - Plant Phenology",
+    {
+        "initial": "Initial growth",
+        "breaking": "Breaking leaf buds",
+        "increasing": "Increasing leaf size",
+        "flowers": "Flowers",
+        "fruiting": "Fruits",
+        "mature": "Leaves",
+        "other": "Other"
+    },
+    "Other"
 )
 
 BUGGY_TRANSFORMERS = [
-    survey_method_transform,
+    survey_transform,
+    development_transform,
+    activity_transform,
+    host_phenology_transform,
 ]
 
 def pull_and_transform_data(kobo: Kobo, uid: str, transformers: list) -> dict:
